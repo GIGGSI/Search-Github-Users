e03220562f22063e0eb2f9fe4daec7892578a68d
Stoyan Spasov <spasovstoyan@gmail.com>
2021-04-13 15:48:37 +0300

    React router setup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="*" component={Error} />
+      </Switch>
+    </Router>
+
   );
 }
 
