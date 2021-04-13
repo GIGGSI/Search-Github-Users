8078a7d17cd5277068637f088b5bec15beead51b
Stoyan Spasov <spasovstoyan@gmail.com>
2021-04-13 22:02:07 +0300

    search functionality is ready

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,10 +5,21 @@ import { GithubContext } from '../context/context';
 
 const Dashboard = () => {
 
+  const { isLoading } = React.useContext(GithubContext);
+
+  if (isLoading) {
+    return <main>
+      <Navbar />
+      <Search />
+      <img src={loadingImage} alt="loading "
+        className="loading-img" />
+
+    </main>
+  }
   return (
     <main>
-      {/* <Navbar />
-      <Search /> */}
+      <Navbar />
+      <Search />
       <Info />
       <User />
       <Repos />
